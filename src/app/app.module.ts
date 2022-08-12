91dff9a8f188af431cb185ea67fa64b027bb454f
cristian <you@example.com>
2022-08-12 18:08:30 -0300

    ultima modificacion 12/08  18.09 hs:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContadorComponent } from './contador/contador.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContadorComponent
+    ContadorComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,49 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   title="Cristian"
   modificado=false
+  classImput=""
+  operacion1=0
+  operacion2=0
+  resultado=0
+  
+  //  p/arrays
+  productos=[
+    {
+      id: 1,
+      Modelo: "Trek 8500",
+      Precio: "1000",
+    },
+    {
+      id: 2,
+      Modelo: "Trek 5600",
+      Precio: "850",
+    }
+  ]
+  // p/strings 
+  categorias=[
+    "MTB",
+    "Ruta"
+  ]
  
   constructor() { }
   cambiarTitle(){
-    this.title = ""
+    if (this.title === ""){
+    this.classImput="error"
+    } else
+    {
+      this.classImput="ready" 
+    }
     this.modificado = true
+  }
+  calcular(){
+    this.resultado= this.operacion1+this.operacion2
+  }
+  add(){
+    this.productos=[{
+      id:3,
+      Modelo: "Trek 9100",
+      Precio: "25000",
+    }]
   }
    
   ngOnInit(): void {
